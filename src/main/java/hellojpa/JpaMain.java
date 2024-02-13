5810397f4705dc0895fb7f94d8fa7cf12b86ee12
a2175 <as217512@gmail.com>
2024-02-13 21:53:20 +0900

    스칼라 타입 프로젝션 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,8 @@ package hellojpa;
 import hellojpa.domain.*;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -13,9 +15,21 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+
         Member member = new Member();
         member.setUsername("member1");
+        member.setAge(10);
         em.persist(member);
+
+        em.flush();
+        em.clear();
+
+        List<MemberDTO> resultList = em.createQuery("SELECT NEW hellojpa.domain.MemberDTO(m.username, m.age) FROM Member m")
+                .getResultList();
+
+        MemberDTO memberDTO = resultList.get(0);
+        System.out.println("username = " + memberDTO.getUsername());
+        System.out.println("age = " + memberDTO.getAge());
 
         tx.commit();
 
